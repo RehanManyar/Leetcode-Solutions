8f196192671d1dab55a5bed70ab6be8b12d10f9e
Rehan Manyar <rehan.manyar@outlook.in>
2026-08-16 13:56:05 +0530

    Time: 43 ms (83.47%), Space: 29 MB (92.61%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // Since array is sorted, no future triplet
+            // can have sum 0 if nums[i] > 0.
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    ans.push_back({
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    // Skip duplicate left values
+                    while (left < right &&
+                           nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right &&
+                           nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    // Move both pointers
+                    left++;
+                    right--;
+                }
+
+                else if (sum < 0) {
+                    // Increase sum
+                    left++;
+                }
+
+                else {
+                    // Decrease sum
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
