0520b03b54c34456959190d00834576f70725b15
Rehan Manyar <rehan.manyar@outlook.in>
2026-08-19 19:58:04 +0530

    Time: 87 ms (26.18%), Space: 86.2 MB (18.58%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,39 +1,58 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        
-        unordered_map<int, int> mp;
 
-        for (auto &x : reservedSeats) {
-            int row = x[0];
-            int seat = x[1];
+        unordered_map<int, unordered_set<int>> mp;
 
-            if (seat >= 2 && seat <= 9) {
-                mp[row] |= (1 << seat);
+        // Store only useful reserved seats
+        for (auto &seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+
+            if (col >= 2 && col <= 9) {
+                mp[row].insert(col);
             }
         }
 
+        // Rows without any important reservation can fit 2 groups
         int ans = (n - mp.size()) * 2;
 
-        for (auto &[row, mask] : mp) {
+        for (auto &[row, seats] : mp) {
 
-            // 2 3 4 5
-            int left = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+            bool left = true;
+            bool middle = true;
+            bool right = true;
 
-            // 4 5 6 7
-            int middle = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+            // Check 2,3,4,5
+            for (int i = 2; i <= 5; i++) {
+                if (seats.count(i)) {
+                    left = false;
+                    break;
+                }
+            }
 
-            // 6 7 8 9
-            int right = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
+            // Check 4,5,6,7
+            for (int i = 4; i <= 7; i++) {
+                if (seats.count(i)) {
+                    middle = false;
+                    break;
+                }
+            }
 
-            bool L = (mask & left) == 0;
-            bool M = (mask & middle) == 0;
-            bool R = (mask & right) == 0;
+            // Check 6,7,8,9
+            for (int i = 6; i <= 9; i++) {
+                if (seats.count(i)) {
+                    right = false;
+                    break;
+                }
+            }
 
-            if (L && R)
+            if (left && right) {
                 ans += 2;
-            else if (L || M || R)
+            }
+            else if (left || middle || right) {
                 ans += 1;
+            }
         }
 
         return ans;
