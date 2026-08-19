65f637c6e10a2ce6e0a68059dafb2fe9c425e94a
Rehan Manyar <rehan.manyar@outlook.in>
2026-08-19 20:01:18 +0530

    Time: 63 ms (40.88%), Space: 86.1 MB (18.58%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,52 +2,59 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
 
-        ranges::sort(reservedSeats);
+        unordered_map<int, unordered_set<int>> mp;
 
-        int families = 2 * n;
-        int m = reservedSeats.size();
+        // Store only useful reserved seats
+        for (auto &seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
 
-        for (int i = 0; i < m; ) {
-
-            int row = reservedSeats[i][0];
-
-            // This row was initially counted as 2 families.
-            families -= 2;
-
-            array<bool, 11> reserved{};
-
-            // Mark reserved seats of this row.
-            while (i < m && reservedSeats[i][0] == row) {
-                reserved[reservedSeats[i][1]] = true;
-                i++;
-            }
-
-            bool left =
-                !reserved[2] &&
-                !reserved[3] &&
-                !reserved[4] &&
-                !reserved[5];
-
-            bool middle =
-                !reserved[4] &&
-                !reserved[5] &&
-                !reserved[6] &&
-                !reserved[7];
-
-            bool right =
-                !reserved[6] &&
-                !reserved[7] &&
-                !reserved[8] &&
-                !reserved[9];
-
-            if (left && right) {
-                families += 2;
-            }
-            else if (left || middle || right) {
-                families += 1;
+            if (col >= 2 && col <= 9) {
+                mp[row].insert(col);
             }
         }
 
-        return families;
+        // Rows without any important reservation can fit 2 groups
+        int ans = (n - mp.size()) * 2;
+
+        for (auto &[row, seats] : mp) {
+
+            bool left = true;
+            bool middle = true;
+            bool right = true;
+
+            // Check 2,3,4,5
+            for (int i = 2; i <= 5; i++) {
+                if (seats.count(i)) {
+                    left = false;
+                    break;
+                }
+            }
+
+            // Check 4,5,6,7
+            for (int i = 4; i <= 7; i++) {
+                if (seats.count(i)) {
+                    middle = false;
+                    break;
+                }
+            }
+
+            // Check 6,7,8,9
+            for (int i = 6; i <= 9; i++) {
+                if (seats.count(i)) {
+                    right = false;
+                    break;
+                }
+            }
+
+            if (left && right) {
+                ans += 2;
+            }
+            else if (left || middle || right) {
+                ans += 1;
+            }
+        }
+
+        return ans;
     }
 };
